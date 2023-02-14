dfb6d3642b327817a28363e28386f8a02515215c
Samuel Lee <slee0258@gmail.com>
2023-02-14 07:54:30 +0000

    In data.js, add a 'beforeunload' event listener to the window object that serializes the data model as JSON and stores the JSON in localStorage.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,8 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('code-journal', dataJSON);
+});
